e680735bd0c28ab665eeb109e25f6013954f9205
loickcherimont <loickcherimont@gmail.com>
2024-10-27 12:26:52 +0100

    Implement JSTL

---
+++ src/main/java/com/example/servlets/Students.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package com.example.servlets;
+
+import com.example.beans.Student;
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author loickcherimont
+ */
+@WebServlet(name = "Students", urlPatterns = {"/students"})
+public class Students extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        // Create 3 students and add them to an array list
+        ArrayList<Student> students = new ArrayList<>();
+
+        students.add(new Student(1, "Théo", "Martins", "theo.martin@hotmail.fr", "0123456789", 22));
+        students.add(new Student(2, "Lionel", "Techer", "lionel.techer@gmail.com", "0987654321", 27));
+        students.add(new Student(3, "Benjamin", "Doe", "doe-benjamin@outlook.fr", "001122334455", 24));
+
+        request.setAttribute("students", students);
+
+        request.getRequestDispatcher("/WEB-INF/jsp/students.jsp").forward(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
