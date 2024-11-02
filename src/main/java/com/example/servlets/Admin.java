9b92de547796ea102886af7f218e9d142fda8b2c
loickcherimont <loickcherimont@gmail.com>
2024-11-02 15:36:29 +0100

    Manage admin account

---
+++ src/main/java/com/example/servlets/Admin.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package com.example.servlets;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author loickcherimont
+ */
+@WebServlet(name = "Admin", urlPatterns = {"/admin"})
+public class Admin extends HttpServlet {
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        Cookie[] cookies = request.getCookies();
+        if(cookies != null) {
+            for(Cookie cookie: cookies) {
+                System.out.printf("Cookie-name: %s | Value: %s\n", cookie.getName(), cookie.getValue());
+                if(cookie.getName().equals("cookieName") && cookie.getValue().equals("foo")) {
+                    request.getRequestDispatcher("/WEB-INF/jsp/admin.jsp").forward(request, response);
+                }
+            }
+            response.sendRedirect("/StudentWorkspace/form");
+            return;
+            
+        } else {
+            response.sendRedirect("/StudentWorkspace/form");
+            return;
+        }
+        
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ src/main/webapp/WEB-INF/jsp/admin.jsp
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Admin</title>
+    </head>
+    <body>
+        <h1>Welcome on your admin session</h1>
+        <%@ include  file="header.jsp" %>
+        <a href="/StudentWorkspace/admin/logout">logout</a>
+    </body>
+</html>
